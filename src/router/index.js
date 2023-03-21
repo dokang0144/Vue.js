823c04ccf77123dcb7a8766ba417e8806247f962
dokang0144 <88234731+dokang0144@users.noreply.github.com>
2023-03-21 10:06:22 +0900

    class8

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/views/PageHome.vue'
+import BoardList from '@/views/board/BoardList.vue'
+import BoardDetail from '@/views/board/BoardDetail.vue'
+import BoardWrite from '@/views/board/BoardWrite.vue'
 
 const routes = [
     {
@@ -9,12 +12,28 @@ const routes = [
     },
     {
         path: '/about',
-        name: 'About',
+        name: 'PageAbout',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/PageAbout.vue')
-    }
+    },
+    {
+        path: '/board/list',
+        name: 'BoardList',
+        component: BoardList
+    },
+    {
+        path: '/board/detail',
+        name: 'BoardDetail',
+        component: BoardDetail
+    },
+    {
+        path: '/board/write',
+        name: 'BoardWrite',
+        component: BoardWrite
+    },
+
 ]
 
 const router = createRouter({
