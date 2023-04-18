34057eb75e7e4f772ed63b8cc991608b2a0bf115
dokang0144 <88234731+dokang0144@users.noreply.github.com>
2023-04-18 11:13:04 +0900

    backup

--- src/views/board/BoardComment.vue
+++ src/views/board/BoardComment.vue
@@ -1,24 +1,24 @@
 <template>
   <Transition duration="550" name="nested">
-  <div class="board-list" v-if="show" id="outer">
-    <div style="border-bottom: 2px solid #92D050; height: 50px;">
+  <div className="board-list">
+    <div style="border-bottom: 2px solid #92D050; height: 50px;" v-if="show" id="outer">
       <p style="float:left; padding: 0 10px 0" id="inner">전체 댓글 <a style="color: dodgerblue;">{{ list.length }}</a>개</p>
     </div>
 
-    <ul className="w3-ul">
-      <li v-for="(row, idx) in list" :key="idx" style="border-bottom: 1px solid #D9D9D9" >
-        <div class="inner" style="float: left; width: 150px;">{{ row.user_name }}</div>
-        <div class="inner">{{ row.comment }}</div>
+    <ul className="w3-ul" v-if="show" id="inner">
+      <li v-for="(row, idx) in list" :key="idx" style="border-bottom: 1px solid #D9D9D9">
+        <div style="float: left; width: 150px;">{{ row.user_name }}</div>
+        <div>{{ row.comment }}</div>
       </li>
     </ul>
     <br>
 
-    <div style="text-align: center; border: solid 2px #E9E5E1; width: 700px; padding: 10px; margin: auto" id="inner">
+    <div style="text-align: center; border: solid 2px #E9E5E1; width: 700px; padding: 10px; margin: auto" >
       <form v-on:submit.prevent="fnSave">
         <input ref="commentInput" v-model="comment" placeholder="댓글을 남겨주세요."
                style="width: 550px; height: 40px; outline: none; border: 0 solid black"
-               type="text">&nbsp;
-        <button className="w3-button w3-green" style="height: 40px" type="submit">댓글쓰기</button>
+               type="text" id="inner">&nbsp;
+        <button className="w3-button w3-green" style="height: 40px" type="submit" id="inner">댓글쓰기</button>
       </form>
     </div><br>
   </div>
@@ -131,18 +131,13 @@ export default {
   opacity: 0.001;
 }
 
-.nested-enter-active .inner,
-.nested-leave-active .inner {
-  transition: all 0.3s ease-in-out;
-}
-/* delay enter of nested element */
-.nested-enter-active .inner {
-  transition-delay: 0.25s;
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
 }
 
-.nested-enter-from .inner,
-.nested-leave-to .inner {
-  transform: translateX(30px);
-  opacity: 0.001;
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
